f3e9f0ed7691d7dd89cb2986810abe130313bd34
Vanhxyz2810 <maitaonoi123@gmail.com>
2025-01-22 03:33:01 +0700

    Update database config again

--- frontend/src/pages/Revenue.tsx
+++ frontend/src/pages/Revenue.tsx
@@ -489,7 +489,7 @@ const Revenue = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Tên Quần Áo</TableCell>
+              <TableCell>Tên Quần Áoo</TableCell>
               <TableCell align="center">Số Lần Thuê</TableCell>
               <TableCell align="right">Doanh Thu</TableCell>
             </TableRow>
